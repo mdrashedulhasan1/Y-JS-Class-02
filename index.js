a4b796aaed1b525b08dfc69585ed8012104bfc6e
Rashedul Hasan <Rashedul Hasan>
2021-11-21 14:05:41 +0600

    JavaScript Code

---
+++ index.js
@@ -0,0 +1,38 @@
+// var a = -4;
+// console.log(Math.abs(a));
+
+// var a = 5.568;
+// console.log(Math.round(a)); //6
+// console.log(Math.ceil(a)); //5
+// console.log(Math.floor(a)); //5
+
+// var mangoPrice = 20;
+// if(mangoPrice!=20){
+//     console.log("Buy Mango");
+// }
+// else{
+//     console.log("Not Buy!")
+// }
+
+
+// var friend1Taka = 80;
+// var friend2Taka = 150;
+// if(friend1Taka>100 && friend2Taka>80){
+//     console.log("Go to restaturant-1");
+// }
+// else if(friend1Taka == 150 || friend2Taka == 150){
+//     console.log("Go to restaturant-2")
+// }
+// else{
+//     console.log("Not go to restaturant")
+// }
+
+// var friendsAge = [17, 23, 22, 21, 19];
+// var rakib = friendsAge[1];
+// friendsAge[1] = 24;
+// console.log(friendsAge);
+// var position = friendsAge.indexOf(24);
+// console.log(position);
+
+
+
